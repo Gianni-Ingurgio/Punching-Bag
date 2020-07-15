636f70cf5a1c6f4f9d49883b1c4f1c611c01ec84
Gianni-Ingurgio <gpingurgio@gmail.com>
2020-07-15 17:05:26 -0400

    Added caps support for prefix

--- bot.js
+++ bot.js
@@ -11,7 +11,7 @@ var prefix = 'bag!'
 var timeout
 
 client.on('message', message => {
-  if (!message.content.startsWith(prefix) || message.author.bot) return;
+  if (!message.content.toLowerCase().startsWith(prefix) || message.author.bot) return;
   else {
 		clearTimeout(timeout)
 		message.guild.member(client.user).setNickname(message.content.slice(prefix.length));
