b3a690cad8ce60195bf8093a41fa114d8e98004f
Gianni-Ingurgio <gpingurgio@gmail.com>
2020-07-16 00:21:46 -0400

    Added user variables

--- README.md
+++ README.md
@@ -1,6 +1,17 @@
 # Punching Bag
-[![Maintenance](https://img.shields.io/badge/Maintained%3F-yes-green.svg?style=flat-square)](https://github.com/Gianni-Ingurgio/Punching-Bag/graphs/commit-activity) [![Issues](https://img.shields.io/github/issues/Gianni-Ingurgio/Punching-Bag?style=flat-square)]() [![HitCount](http://hits.dwyl.com/Gianni-Ingurgio/Punching-Bag.svg)](http://hits.dwyl.com/Gianni-Ingurgio/Punching-Bag)
+[![Maintenance](https://img.shields.io/badge/Maintained%3F-yes-green.svg?style=flat-square)](https://github.com/Gianni-Ingurgio/Punching-Bag/graphs/commit-activity) [![Issues](https://img.shields.io/github/issues/Gianni-Ingurgio/Punching-Bag?style=flat-square)]() [![HitCount](http://hits.dwyl.com/Gianni-Ingurgio/Punching-Bag.svg)](http://hits.dwyl.com/Gianni-Ingurgio/Punching-Bag) [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg?style=flat-square)](./LICENSE.md)
 ## Installation
 1. Clone or download wherever you want it
-2. Create a token.json file with your [bot token](https://www.writebots.com/discord-bot-token/) `"n3vEr_5h4r3.y0ur-b07.70k3n_w17h-4ny0n3"`
-3. Run bot.js
+2. Install dependencies with `npm i`
+3. Create a token.json file with your [bot token](https://www.writebots.com/discord-bot-token/) `"n3vEr_5h4r3.y0ur-b07.70k3n_w17h-4ny0n3"`
+4. Run with `node .`
+
+## Variables
+Settings can be found at the top of the bot.js file
+```javascript
+///////////// User variables here /////////////
+var
+prefix = '!bag',  //set prefix
+reset = true,     //toggle auto reset
+ms = 60000        //set delay in ms to reset
+```
--- bot.js
+++ bot.js
@@ -1,25 +1,32 @@
+///////////// User variables here /////////////
+var
+prefix = '!bag',  //set prefix
+reset = true,     //toggle auto reset
+ms = 60000        //set delay in ms to reset
+//////////// Proceed with caution /////////////
+
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const token = require('./token.json'); //"[token]"
 
 client.on('ready', () => {
-	client.user.setPresence({activity: {type: 'WATCHING', name: 'bag! [victim]'}})
-	console.log(client.user.username, 'ready');
+	client.user.setPresence({activity: {type: 'WATCHING', name: prefix + ' [victim]'}})
+	console.log(client.user.username, 'watching', prefix);
+	console.log('Set to autoreset after', ms/1000, 'seconds')
 });
 
-var prefix = 'bag!'
 var timeout
 
 client.on('message', message => {
   if (!message.content.toLowerCase().startsWith(prefix) || message.author.bot) return;
   else {
-		clearTimeout(timeout)
+		if (reset) clearTimeout(timeout)
 		message.guild.member(client.user).setNickname(message.content.slice(prefix.length));
-		console.log(message.author.tag + ' : ' + message.content.slice(prefix.length));
-		timeout = setTimeout(function () {
-			message.guild.member(client.user).setNickname('')
-			console.log('Automatically reset');
-		}, 60000);
+		console.log(message.guild.name + ' : ' + message.author.tag + ' : ' + message.content.slice(prefix.length));
+		if (reset) { timeout = setTimeout(function () {
+				message.guild.member(client.user).setNickname('')
+				console.log('Automatically reset after ' + ms / 1000 + ' seconds');
+		}, ms)};
 	}
 })
 
